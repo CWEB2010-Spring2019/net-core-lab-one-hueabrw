fdeb0452553703154b51a3e2fc75d87c78ff2be4
Abraham Huerta <Abrhuerta@gmail.com>
2019-01-15 17:00:43 -0600

    finished!

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -7,6 +7,7 @@ namespace lab_one
     {
         static void Main(string[] args)
         {
+            //Declerations
             string[] AnswerKey = { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" };
             string[] quizQuestions = { "when was .NET core released?", "What is the current version of .NET core?", "which operating system does NOT support .NET Core?",
                                         ".NET project was created by...", "What languages can be used to write applications and libraries for .NET Core?",
@@ -31,52 +32,122 @@ namespace lab_one
 
             ConsoleKey keyPressed = new ConsoleKey();
 
-            Console.WriteLine("Press ENTER to continue...");
+            bool quit = false;
 
-            while(keyPressed != ConsoleKey.Enter)
+            //Main code starts here
+            while (!quit)
             {
-                keyPressed = Console.ReadKey().Key;
-            }
-
-            for (int i = 0; i < quizQuestions.Length; i++)
-            {
-                Console.Clear();
-                Console.WriteLine(quizQuestions[i]);
-                for (int j = 0; j < 4; j++)
+                if (keyPressed != ConsoleKey.Enter)
                 {
-                    Console.Write(options[j]);
-                    Console.WriteLine(quizOptions[i, j]);
-                }
-                Console.WriteLine("Please enter an option");
-                userSelection[i] = Console.ReadLine();
-                if(userSelection[i] == AnswerKey[i])
-                {
-                    Console.Clear();
-                    Console.BackgroundColor = ConsoleColor.DarkGreen;
-                    Console.WriteLine("Correct!");
-                    System.Threading.Thread.Sleep(500);
+                    Console.WriteLine("Press ENTER to start...");
+                    keyPressed = Console.ReadKey().Key;
                 }
                 else
                 {
+                    for (int i = 0; i < quizQuestions.Length; i++)
+                    {
+                        //Presents Questions
+                        Console.Clear();
+                        Console.WriteLine(quizQuestions[i]);
+                        //Presents Multiple Choice Options
+                        for (int j = 0; j < quizOptions.GetLength(1); j++)
+                        {
+                            Console.Write(options[j]);
+                            Console.WriteLine(quizOptions[i, j]);
+                        }
+                        //Asks for user input
+                        Console.WriteLine("Please enter an option");
+                        userSelection[i] = Console.ReadLine().ToUpper();
+
+                        //Flashes the screen green and says correct if they're right
+                        if (userSelection[i] == AnswerKey[i])
+                        {
+                            Console.Clear();
+                            Console.BackgroundColor = ConsoleColor.DarkGreen;
+                            Console.WriteLine("Correct!");
+                            System.Threading.Thread.Sleep(500);
+                        }
+                        //Flashes the screen red and says incorrect if they're wrong
+                        else
+                        {
+                            Console.Clear();
+                            Console.BackgroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine("Sorry, incorrect.");
+                            System.Threading.Thread.Sleep(500);
+                        }
+                        //resets the background to black
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+
+                    //counting number they got right
+                    int numberCorrect = 0;
+
+                    //Presents their answers and the asnwer key side by side
                     Console.Clear();
-                    Console.BackgroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("Sorry, incorrect.");
-                    System.Threading.Thread.Sleep(500);
-                }
-                Console.BackgroundColor = ConsoleColor.Black;
-            }
-            /*
-            string[] scramble(char[] opArray, string[] questionOp)
-            {
-                Random rand = new Random();
-                string[] newOptions = new string[4];
-                for (int i = 0; i < 4; i++)
-                {
-                    
+                    Console.WriteLine("\n\t|Question|Your Answer|Correct Answer|");
+                    for(int i = 0; i < quizQuestions.Length; i++)
+                    {
+                        Console.Write("\t");
+                        //Set background to green if they're right
+                        if (userSelection[i] == AnswerKey[i])
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGreen;
+                            numberCorrect++;
+                        }
+                        //Set background to red if they're wrong
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkRed;
+                        }
+                        //formatting code
+                        string num = "";
+                        if(i < 9)
+                        {
+                            num = "0";
+                        }
+
+                        Console.WriteLine("|"+ num + (i+1) + "      |" + userSelection[i] + "          |" + AnswerKey[i] + "             |");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+
+                    //resets the background to black
+
+                    float userPercentege = (float)numberCorrect / (float)quizQuestions.Length;
+                    Console.WriteLine("\nYou got " + (userPercentege * 100.00f) + "% correct.");
+                    if(userPercentege < 0.7f)
+                    {
+                        Console.WriteLine("You failed.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Congratulations, you passed!");
+                    }
+
+                    bool answered = false;
+
+                    while (!answered)
+                    {
+                        Console.WriteLine("Do you want to try again?(Y/N)");
+                        keyPressed = Console.ReadKey().Key;
+                        Console.WriteLine();
+                        if (keyPressed == ConsoleKey.Y)
+                        {
+                            answered = true;
+                            Console.Clear();
+                        }else if(keyPressed == ConsoleKey.N)
+                        {
+                            answered = true;
+                            quit = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not a valid answer...");
+                        }
+                    }
+
                 }
 
-                return newOptions;
-            }*/
+            }
         }
     }
 }
